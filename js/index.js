3aabc08708bb0750ae3b92f45e5791020a2b5349
Meisha Hill <meishahill@gmail.com>
2015-11-13 10:17:44 -0500

    update to other.css to make nag the same

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
 
-// $(function(){
-//   $(".burger").click(function() {
-//     $("ul .nav").slideToggle();
-//   });
-
-// });
-
 $(function(){
-  $("#hamburger").click(function(){
-    if(!$("#hamburger").parent().hasClass("slideDown")){
-      $("#hamburger").parent().addClass("slideDown");
-    } else {
-      $("#hamburger").parent().removeClass("slideDown");
-    }
-  })
-})
+
+  $(".submenu").click(function() {
+     // console.log('nav was clicked');
+     $(".nav").slideToggle();
+  });
+
+  $("li").click(function(){
+          console.log('link was clicked');
+          //remove the active class from all links
+          $('li a').removeClass("active");
+          //add active class to the link that was selected
+          $(this).addClass("active");
+  });
+
+
+
+// $(function(){
+//   $(".submenu").click(function(){
+//     if(!$(".nav").hasClass("slideUp")){
+//       $(".nav").addClass("slideUp");
+//     } else {
+//       $(".nav").removeClass("slideUp");
+//     }
+//   })
+// })
+
+});
