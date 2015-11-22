1f25a93488ffdeb990bc652e55581eb66fcf56c3
Meisha Hill <meishahill@gmail.com>
2015-11-22 17:00:36 -0500

    Updated product page with interactive reveal
    Still has old html for product page

--- js/index.js
+++ js/index.js
@@ -4,26 +4,16 @@ $(function(){
   $(".submenu").click(function() {
      // console.log('nav was clicked');
      $(".nav").slideToggle();
+    $("ul").append('<li><a href="https://instagram.com/marylouise0701/" target="_blank">Instagram</a></li>');
+    $("ul").append('<li><a href="http://www.fjuled.com/blog/" target="_blank">Blog</a></li>');
   });
 
-  $("li").click(function(){
-          console.log('link was clicked');
-          //remove the active class from all links
-          $('li a').removeClass("active");
-          //add active class to the link that was selected
-          $(this).addClass("active");
-  });
-
-
-
-// $(function(){
-//   $(".submenu").click(function(){
-//     if(!$(".nav").hasClass("slideUp")){
-//       $(".nav").addClass("slideUp");
-//     } else {
-//       $(".nav").removeClass("slideUp");
-//     }
-//   })
-// })
+  $(".hover").mouseleave(
+    function() {
+      $(this).removeClass("hover");
+    }
+  );
+ 
 
 });
+
